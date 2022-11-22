3e7be0f945509efc3ba36d4aa7feb247023ba1fd
Your Name <you@example.com>
2022-11-22 16:49:18 +0300

    Реализация валидации на js

--- kursach/static/kursach/js/add_transaction.js
+++ kursach/static/kursach/js/add_transaction.js
@@ -1,11 +1,12 @@
 import Datepicker from './vanillajs-datepicker/js/Datepicker.js'
 import ru from './vanillajs-datepicker/js/i18n/locales/ru.js';
 import {dateString} from "./custom_format_for_date.js";
-import {checkRequired} from "./forms_validations.js";
+// import {checkRequired} from "./forms_validations.js";
 
 window.addEventListener('load', (event) => {
     const doc = document;
     const addNewTransBtn = doc.querySelector('#add-transaction-btn');
+    const addNewTransBtnSide = doc.querySelector('#add-new-transaction');
     const rowCol = doc.querySelector('.row-col');
     const transactionAddForm = doc.querySelector('#add-transaction');
     const elem = doc.getElementById('date');
@@ -22,17 +23,18 @@ window.addEventListener('load', (event) => {
       language: 'ru'
     });
 
-
-
-    addNewTransBtn.addEventListener('click', ()=>{
-        checkRequired();
-        const categoryButtons = doc.querySelectorAll('.category')
-        categoryButtons.forEach(btn => btn.classList.toggle('hidden'))
+    function newTransaction(){
+        const categoryButtons = doc.querySelectorAll('.category');
+        categoryButtons.forEach(btn => btn.classList.toggle('hidden'));
         addNewTransBtn.classList.toggle('hidden');
         transactionAddForm.classList.toggle('hidden');
         rowCol.classList.toggle('hidden');
-        getTypes()
-    })
+        getTypes();
+    }
+
+    addNewTransBtnSide.addEventListener('click', newTransaction)
+
+    addNewTransBtn.addEventListener('click', newTransaction)
 
     async function getTypes(){
         const types = await fetch(`${url}${TypeOfTransactionApiList}`)
@@ -49,23 +51,45 @@ window.addEventListener('load', (event) => {
     }
 
 
-    formSubmitBtn.addEventListener('click', (e)=>{
-        e.preventDefault()
-        const form = {
-            'name':doc.querySelector('#name-trans').value,
-            'username': doc.querySelector('#username').value,
-            'category': +doc.querySelector('#category_id').value,
-            'type':+doc.querySelector('#type_id').value,
-            'price':parseFloat(doc.querySelector('#sum').value.replace(",", ".")),
-            'date': dateString(doc.querySelector('#date').value),
-            'user': +doc.querySelector('#userId').value
-        }
-        try{
-            sendRequest('POST', `${url}${transApiUrl}`, form);
-        }catch (e){
-            rowCol.innerHTML += `<h1>${e}</h1>`
-        }
-    })
+    // formSubmitBtn.addEventListener('click', (e)=>{
+    //     e.preventDefault()
+    //     const form = {
+    //         'name':doc.querySelector('#name-trans').value,
+    //         'username': doc.querySelector('#username').value,
+    //         'category': +doc.querySelector('#category_id').value,
+    //         'type':+doc.querySelector('#type_id').value,
+    //         'price':parseFloat(doc.querySelector('#sum').value.replace(",", ".")),
+    //         'date': dateString(doc.querySelector('#date').value),
+    //         'user': +doc.querySelector('#userId').value
+    //     }
+    //     try{
+    //         sendRequest('POST', `${url}${transApiUrl}`, form);
+    //     }catch (e){
+    //         rowCol.innerHTML += `<h1>${e}</h1>`
+    //     }
+    // })
+
+
+
+
+
+    // transactionAddForm.addEventListener('submit', (e)=>{
+    //     e.preventDefault()
+    //     const form = {
+    //         'name':doc.querySelector('#name-trans').value,
+    //         'username': doc.querySelector('#username').value,
+    //         'category': +doc.querySelector('#category_id').value,
+    //         'type':+doc.querySelector('#type_id').value,
+    //         'price':parseFloat(doc.querySelector('#sum').value.replace(",", ".")),
+    //         'date': dateString(doc.querySelector('#date').value),
+    //         'user': +doc.querySelector('#userId').value
+    //     }
+    //     try{
+    //         sendRequest('POST', `${url}${transApiUrl}`, form);
+    //     }catch (e){
+    //         rowCol.innerHTML += `<h1>${e}</h1>`
+    //     }
+    // })
 
 
     function getCookie(name) {
@@ -98,23 +122,18 @@ window.addEventListener('load', (event) => {
             titleCheck ? titleCheck.innerHTML = '' : {}
             console.log(response.status);
             if (response.ok && response.status<300){
-                console.log(111)
                 const h2 = doc.createElement('h2');
-                console.log(h2)
                 h2.innerHTML = `<h2 class="alert alert-success">Успешно!</h2>`;
                 const formDiv = doc.querySelector('.form-div');
                 formDiv.insertBefore(h2, formDiv.firstChild);
                 return response.json()
             }else{
-                const h2 = doc.createElement('h2');
-                h2.innerHTML = `<h2 class="alert alert-danger">Неверно заполнена форма!</h2>`;
-                const formDiv = doc.querySelector('.form-div');
-                formDiv.insertBefore(h2, formDiv.firstChild);
+                console.log('error send')
             }
         })
     }
 
-
+    //
     // sendRequest('POST', `${url}${transApiUrl}`, )
     //     {
     //     "date": "2022-11-17",
@@ -125,6 +144,67 @@ window.addEventListener('load', (event) => {
     //     "username": "Ваня",
     //     "user": 1
     // }
+
+    const form  = doc.getElementsByTagName('form')[0];
+
+    const name = doc.querySelector('#name-trans');
+    const nameError = doc.querySelector('#name-trans + span.error');
+
+    const username = doc.querySelector('#username');
+    const usernameError = doc.querySelector('#username + span.error');
+
+    const type = doc.querySelector('#type_id');
+    const typeError = doc.querySelector('#type_id + span.error');
+
+    const category = doc.querySelector('#category_id');
+    const categoryError = doc.querySelector('#category_id + span.error');
+
+    const sum = doc.querySelector('#sum');
+    const sumError = doc.querySelector('#sum + span.error');
+
+    const date = doc.querySelector('#date');
+    const dateError = doc.querySelector('#date + span.error');
+
+    const formInputs = [[name, nameError], [username, usernameError], [type, typeError], [category, categoryError], [sum, sumError], [date, dateError]];
+    formInputs.forEach(item=>item[0].addEventListener('input', function (event) {
+      if (item[0].validity.valid) {
+        item[1].textContent = '';
+        item[1].className = 'error';
+      } else {
+        showError();
+      }
+    }))
+
+    form.addEventListener('submit', function (event) {
+      formInputs.forEach(item=>{
+        if (!item[0].validity.valid){
+          showError(item[0], item[1]);
+          event.preventDefault();
+        }else{
+            event.preventDefault()
+            const form = {
+                'name':doc.querySelector('#name-trans').value,
+                'username': doc.querySelector('#username').value,
+                'category': +doc.querySelector('#category_id').value,
+                'type':+doc.querySelector('#type_id').value,
+                'price':parseFloat(doc.querySelector('#sum').value.replace(",", ".")),
+                'date': dateString(doc.querySelector('#date').value),
+                'user': +doc.querySelector('#userId').value
+            }
+            sendRequest('POST', `${url}${transApiUrl}`, form);
+
+        }
+      })
+    });
+
+    function showError(field, fieldError) {
+        if (field.validity.valueMissing){
+          fieldError.textContent = 'Пожалуйста, заполните поле';
+        }else if(field.validity.typeMismatch) {
+          fieldError.textContent = 'Убедитесь в правильности ввода';
+      }
+      fieldError.className = 'error active';
+    }
 
 })
 
--- kursach/static/kursach/js/forms_validations.js
+++ kursach/static/kursach/js/forms_validations.js
@@ -1,15 +1,53 @@
+//
+// const doc = document;
+// const form  = doc.getElementsByTagName('form')[0];
+//
+// const name = doc.querySelector('#name-trans');
+// const nameError = doc.querySelector('#name-trans + span.error');
+//
+// const username = doc.querySelector('#username');
+// const usernameError = doc.querySelector('#username + span.error');
+//
+// const type = doc.querySelector('#type_id');
+// const typeError = doc.querySelector('#type_id + span.error');
+//
+// const category = doc.querySelector('#category_id');
+// const categoryError = doc.querySelector('#category_id + span.error');
+//
+// const sum = doc.querySelector('#sum');
+// const sumError = doc.querySelector('#sum + span.error');
+//
+// const date = doc.querySelector('#date');
+// const dateError = doc.querySelector('#date + span.error');
+//
+// const formInputs = [[name, nameError], [username, usernameError], [type, typeError], [category, categoryError], [sum, sumError], [date, dateError]];
+// formInputs.forEach(item=>item[0].addEventListener('input', function (event) {
+//   if (item[0].validity.valid) {
+//     item[1].textContent = '';
+//     item[1].className = 'error';
+//   } else {
+//     showError();
+//   }
+// }))
+//
+// form.addEventListener('submit', function (event) {
+//   formInputs.forEach(item=>{
+//     if (!item[0].validity.valid){
+//       showError(item[0], item[1]);
+//       event.preventDefault();
+//     }
+//   })
+// });
+//
+// function showError(field, fieldError) {
+//     if (field.validity.valueMissing){
+//       fieldError.textContent = 'Пожалуйста, заполните поле';
+//     }else if(field.validity.typeMismatch) {
+//       fieldError.textContent = 'Убедитесь в правильности ввода';
+//   }
+//   fieldError.className = 'error active';
+//
+// }
 
 
-export function checkRequired(){
-    const formItems = document.querySelectorAll('.form-control');
-    console.log(formItems);
-    formItems.forEach(item=>{
-        item.addEventListener('change', ()=>{
-            if (!item.value){
-                item.classList.add('invalid');
-            }else{
-                item.classList.remove('invalid')
-            }
-        })
-    })
-}
+
